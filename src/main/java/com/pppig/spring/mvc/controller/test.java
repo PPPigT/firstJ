00711da218de76a2ddc29db1704d0431047c186a
PPPigT <110145626+PPPigT@users.noreply.github.com>
2022-07-28 17:53:06 +0800

    第二次测试

--- src/main/java/com/pppig/spring/mvc/controller/test.java
+++ src/main/java/com/pppig/spring/mvc/controller/test.java
@@ -3,5 +3,6 @@ package com.pppig.spring.mvc.controller;
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("第二次测试");
     }
 }
